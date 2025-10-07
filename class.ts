7f33e13653dfd7ff6a8149842cb8d0b8d5f1331f
Tran Thi Thao Nhi <tran.thi.thao.nhi@sun-asterisk.com>
2025-10-07 16:14:18 +0700

    Class & interface

---
+++ class.ts
@@ -0,0 +1,23 @@
+interface IUser{
+name: string,
+email: string,
+isAdmin: boolean
+}
+
+class User implements IUser{
+name: string;
+email: string;
+isAdmin: boolean;
+
+public constructor(name: string, email: string, isAdmin: boolean){
+this.name = name;
+this.email = email;
+this.isAdmin = isAdmin;
+}
+public getInfo(): string{
+return `User: ${this.name}, Email: ${this.email}, Admin: ${this.isAdmin}`;
+}
+}
+
+const user = new User("Nhi", "tran.thi.thao.nhi@sun-asterisk.com", true);
+console.log(user.getInfo());
