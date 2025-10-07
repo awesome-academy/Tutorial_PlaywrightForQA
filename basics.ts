86f919a25ebd146b191ff87842cd8d6ef1a3ecac
nhungnth-2222 <nguyen.thi.hong.nhung-b@sun-asterisk.com>
2025-10-07 22:51:12 +0700

    done BTTH1

---
+++ basics.ts
@@ -0,0 +1,9 @@
+let username = "nhungbnth"
+let age = 28
+let isActive = true
+let roles = ["admin","user"]
+let user = {name:"nhungbnth",email:"nhungnth150798@gmail.com", isAdmin:true}
+console.log(`User: ${username} (email: ${user.email}), Roles: ${roles}, Active: ${isActive}
+`)
+
+
