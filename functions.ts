9942435f8e120fa92038b1e545554897b8eafaa0
nhungnth-2222 <nguyen.thi.hong.nhung-b@sun-asterisk.com>
2025-10-07 22:51:27 +0700

    done BTTH2

---
+++ functions.ts
@@ -0,0 +1,14 @@
+function sum(a:number, b: number)
+{
+    return a+b;
+}
+
+const  multiply = (a: number, b: number): number => a*b;
+
+function greet(name: string, role: string = "Guest")
+{
+    console.log(`"Hello ${name}, your role is ${role}"`)
+}
+
+
+
