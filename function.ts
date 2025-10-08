e524d99a5723460f81e8af418223ae6fd5e0b804
Tran Thi Thao Nhi <tran.thi.thao.nhi@sun-asterisk.com>
2025-10-08 15:25:11 +0700

    Fix end-line & indent

--- function.ts
+++ function.ts
@@ -1,20 +1,20 @@
-function sum(a: number, b: number){
-return a + b;
+function sum(a: number, b: number) {
+  return a + b;
 }
 console.log(sum(2, 3));
 
 const multiply = (a: number, b: number): number => {
   return a * b;
-}
+};
 console.log(multiply(2, 3));
 
-function greet(name: string, role: string = "Guest"){
-return `Hello ${name}, your role ${role}`;
+function greet(name: string, role: string = "Guest") {
+  return `Hello ${name}, your role ${role}`;
 }
 console.log(greet("Nhi"));
 
 function delay(ms: number): Promise<void> {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 async function delayPrint(msg: string, time: number): Promise<void> {
   await delay(time);
