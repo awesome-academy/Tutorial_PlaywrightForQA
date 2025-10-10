26815c5d1ca974000dae761c11ae1a198a14f880
nahth-2927 <huynh.thi.hong.na@sun-asterisk.com>
2025-10-10 20:07:47 +0900

    Add: BTTH function_arrow function

---
+++ function.ts
@@ -0,0 +1,21 @@
+// Tính tổng a và b
+function sum (a: number, b: number) {
+    return a+b
+};
+console.log(sum(10, 3));
+
+//viết arrow function multiple
+const multiple = (a: number, b: number) => {
+ return a*b
+};
+console.log(multiple(10, 3));
+
+//viết function greet(name: string, role: string = "Guest")
+
+function greet(name: string, role: string = "Guest") {
+}
+console.log(`Hello ${greet.name}, your role is ${greet.role = "admin"}`);
+
+//
+
+
